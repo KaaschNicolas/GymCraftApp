7cea588cbb53f2d1e34e70552d1c40fd4a2b1bad
Andreas Kovacs <ak225es@student.lnu.se>
2024-01-03 15:12:14 +0100

    added activity membership and button to said activity

--- app/src/main/java/com/example/myapplication/ProfileFragment.kt
+++ app/src/main/java/com/example/myapplication/ProfileFragment.kt
@@ -1,23 +1,37 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.viewbinding.ViewBindings
+import com.example.myapplication.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    private lateinit var binding: Button
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-        ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.mitgliedschaftButton.setOnClickListener{
+            val intent = Intent(requireContext(), MembershipActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
