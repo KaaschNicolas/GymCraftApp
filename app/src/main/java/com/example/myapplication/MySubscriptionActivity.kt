da3b047f30ee14bc8f1d5dd7965aa2dc27b76631
Andreas Kovacs <ak225es@student.lnu.se>
2024-01-07 15:03:58 +0100

    Added adapter for subscriptions

--- app/src/main/java/com/example/myapplication/MySubscriptionActivity.kt
+++ app/src/main/java/com/example/myapplication/MySubscriptionActivity.kt
@@ -2,15 +2,26 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.adapters.SubscriptionListAdapter
+import com.example.myapplication.daos.TariffDao
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.databinding.ActivityMembershipBinding
 import com.example.myapplication.databinding.ActivityMySupscriptionsBinding
 
 class MySubscriptionActivity : AppCompatActivity() {
+    private lateinit var listView: ListView
+    private lateinit var adapter: SubscriptionListAdapter
+    private lateinit var tariffDao: TariffDao
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_supscriptions);
+
+        listView = findViewById(R.id.subscriptionLv)
+        adapter = SubscriptionListAdapter(this, ArrayList())
+        listView.adapter = adapter
     }
 }
