339a02a2f2682d6b2c3bb5137f7fa583834fde97
ucuxp <ucuxp@student.kit.edu>
2024-01-08 19:40:02 +0100

    added capacity colour and changed design

--- app/src/main/java/com/example/myapplication/StudioFragment.kt
+++ app/src/main/java/com/example/myapplication/StudioFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.graphics.Color
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.viewmodel.StudioViewModel
@@ -29,10 +30,33 @@ class StudioFragment(
             view.findViewById<TextView>(R.id.openingHours).text = it.openingHours
             view.findViewById<TextView>(R.id.description).text = it.description
 
-        val randomAuslastung = Random.nextInt(0, 301)
-        val auslastungText = "$randomAuslastung/300"
-        view.findViewById<TextView>(R.id.auslastung).text = auslastungText
-    }
+            val randomAuslastung = Random.nextInt(0, 301)
+            val auslastungText = "$randomAuslastung/300"
+            view.findViewById<TextView>(R.id.auslastung).text = auslastungText
+            val auslastungTextView = view.findViewById<TextView>(R.id.auslastung)
+            auslastungTextView.text = auslastungText
+
+            when {
+                randomAuslastung >= 0 && randomAuslastung <= 100 -> {
+
+                    auslastungTextView.setBackgroundColor(Color.GREEN)
+                }
+
+                randomAuslastung > 100 && randomAuslastung <= 200 -> {
+
+                    auslastungTextView.setBackgroundColor(Color.YELLOW)
+                }
+
+                randomAuslastung > 200 && randomAuslastung <= 300 -> {
+
+                    auslastungTextView.setBackgroundColor(Color.RED)
+                }
+
+                else -> {
+                    auslastungTextView.setBackgroundColor(Color.TRANSPARENT)
+                }
+            }
+        }
     return view
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
